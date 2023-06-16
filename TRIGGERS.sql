332cc6fcdfaf90deb47689359ac977d13fecae6a
PinkDolphin343 <88987822+PinkDolphin343@users.noreply.github.com>
2023-06-15 20:46:51 -0600

    Update TRIGGERS.sql

--- TRIGGERS.sql
+++ TRIGGERS.sql
@@ -7,11 +7,18 @@ AS
 BEGIN
   SET NOCOUNT ON;
 
+  -- Declarar variables
+  DECLARE @ID_C INT, @NSS VARCHAR(20), @Costo DECIMAL(10, 2);
+
+  -- Obtener valores de la fila insertada
+  SELECT @ID_C = ID_C, @NSS = NSS, @Costo = COSTO
+  FROM inserted;
+
   -- Insertar valores en Historial_clinico
   INSERT INTO Historial_clinico (ID_C, NSS, Costo)
-  SELECT inserted.ID_C, inserted.NSS, inserted.COSTO
-  FROM inserted;
+  VALUES (@ID_C, @NSS, @Costo);
 END;
+
 
 --Este nuevo trigger se ejecutará después de que se realice una inserción en la tabla "Receta". Actualiza los valores de "Diagnistico", "Fecha_creacion" y "Observaciones" en la tabla "Historial_clinico" 
 --basándose en la coincidencia de "ID_C" y "NSS" entre la tabla "Historial_clinico" y la tabla "Cita
@@ -23,15 +30,18 @@ AS
 BEGIN
   SET NOCOUNT ON;
 
-  -- Insertar valores en Historial_clinico
+  -- Declarar variables
+  DECLARE @ID_C INT, @NSS VARCHAR(20), @Diagnostico VARCHAR(100), @FechaCreacion DATE, @Observaciones VARCHAR(200);
+
+  -- Obtener valores de la fila insertada en Receta
+  SELECT @ID_C = r.ID_C, @NSS = c.NSS, @Diagnostico = r.Diagnistico, @FechaCreacion = r.Fecha_creacion, @Observaciones = r.Observaciones
+  FROM inserted AS r
+  JOIN Cita AS c ON r.ID_C = c.ID_C;
+
+  -- Actualizar valores en Historial_clinico
   UPDATE Historial_clinico
-  SET Diagnistico = inserted.Diagnistico,
-      Fecha_creacion = inserted.Fecha_creacion,
-      Observaciones = inserted.Observaciones
-  FROM Historial_clinico
-  JOIN Cita ON Historial_clinico.ID_C = Cita.ID_C
-  WHERE Historial_clinico.NSS = Cita.NSS
-    AND Historial_clinico.ID_C = inserted.ID_C;
+  SET Diagnistico = @Diagnostico, Fecha_creacion = @FechaCreacion, Observaciones = @Observaciones
+  WHERE ID_C = @ID_C AND NSS = @NSS;
 END;
 
 
@@ -39,12 +49,18 @@ END;
 --Este trigger se ejecutará después de cada actualización en la tabla "Historial_clinico". Verifica si el campo "horario" en la fila actualizada es nulo y, si es así, actualiza el campo "horario" con la fecha y hora actual.
 
 CREATE TRIGGER ActualizarHorario
-AFTER UPDATE ON Historial_clinico
-FOR EACH ROW
+ON Historial_clinico
+AFTER UPDATE
+AS
 BEGIN
-  IF NEW.horario IS NULL THEN
+  SET NOCOUNT ON;
+
+  -- Verificar si el campo "horario" en la fila actualizada es nulo
+  IF EXISTS (SELECT horario FROM inserted WHERE horario IS NULL)
+  BEGIN
+    -- Actualizar el campo "horario" con la fecha y hora actual
     UPDATE Historial_clinico
     SET horario = CONVERT(TIME, GETDATE())
-    WHERE No_HC = NEW.No_HC;
-  END IF;
+    WHERE No_HC IN (SELECT No_HC FROM inserted WHERE horario IS NULL);
+  END;
 END;
