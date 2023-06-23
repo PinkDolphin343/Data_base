60b6fb82b7ead6ab0e98fdb036ed8574860972fe
PinkDolphin343 <88987822+PinkDolphin343@users.noreply.github.com>
2023-06-23 17:43:18 -0600

    Create FUNCION-PARA-RECETAS.sql

---
+++ FUNCION-PARA-RECETAS.sql
@@ -0,0 +1,35 @@
+-- ================================================
+-- Template generated from Template Explorer using:
+-- Create Inline Function (New Menu).SQL
+--
+-- Use the Specify Values for Template Parameters 
+-- command (Ctrl-Shift-M) to fill in the parameter 
+-- values below.
+--
+-- This block of comments will not be included in
+-- the definition of the function.
+-- ================================================
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+-- =============================================
+-- Author:		<Author,,Name>
+-- Create date: <Create Date,,>
+-- Description:	<Description,,>
+-- =============================================
+
+CREATE FUNCTION ObtenerInformacionReceta (@IDReceta INT)
+RETURNS TABLE
+AS
+RETURN
+(
+    SELECT Receta.ID_R, Receta.Diagnostico, Receta.Fecha_creacion, Receta.Observaciones,
+           Receta.Cedula, Receta.NSS, Medicamentos.ID_Medicamento, Medicamentos.Nombre_Medicamento,
+           Presentacion.Presentacion
+    FROM Receta
+    INNER JOIN Receta_Medicamento ON Receta.ID_R = Receta_Medicamento.ID_R
+    INNER JOIN Medicamentos ON Receta_Medicamento.ID_Medicamento = Medicamentos.ID_Medicamento
+    INNER JOIN Presentacion ON Medicamentos.ID_Presentacion = Presentacion.ID_P
+    WHERE Receta.ID_R = @IDReceta
+)
