f6401c4523637e12ed65d51fd72062ddd56970e4
PinkDolphin343 <88987822+PinkDolphin343@users.noreply.github.com>
2023-06-23 17:57:11 -0600

    Update FUNCION-PARA-RECETAS.sql

--- FUNCION-PARA-RECETAS.sql
+++ FUNCION-PARA-RECETAS.sql
@@ -18,18 +18,20 @@ GO
 -- Create date: <Create Date,,>
 -- Description:	<Description,,>
 -- =============================================
-
 CREATE FUNCTION ObtenerInformacionReceta (@IDReceta INT)
 RETURNS TABLE
 AS
 RETURN
 (
     SELECT Receta.ID_R, Receta.Diagnostico, Receta.Fecha_creacion, Receta.Observaciones,
-           Receta.Cedula, Receta.NSS, Medicamentos.ID_Medicamento, Medicamentos.Nombre_Medicamento,
-           Presentacion.Presentacion
+           CONCAT(Paciente.Nombre, ' ', Paciente.A_paterno, ' ', Paciente.A_materno) AS NombrePaciente,
+           CONCAT(Medico.Nombre, ' ', Medico.A_Paterno, ' ', Medico.A_materno) AS NombreMedico,
+           Medicamentos.ID_Medicamento, Medicamentos.Nombre_Medicamento, Presentacion.Presentacion
     FROM Receta
     INNER JOIN Receta_Medicamento ON Receta.ID_R = Receta_Medicamento.ID_R
     INNER JOIN Medicamentos ON Receta_Medicamento.ID_Medicamento = Medicamentos.ID_Medicamento
     INNER JOIN Presentacion ON Medicamentos.ID_Presentacion = Presentacion.ID_P
+    INNER JOIN Paciente ON Receta.NSS = Paciente.NSS
+    INNER JOIN Medico ON Receta.Cedula = Medico.Cedula
     WHERE Receta.ID_R = @IDReceta
 )
